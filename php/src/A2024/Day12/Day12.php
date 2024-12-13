9d81f8a07667f199c7f478b1028e052ea87da35f
FactorSpeed <alexis25.py@gmail.com>
2024-12-13 16:19:56 +0100

    [2024][Day12] PHP Part2

--- php/src/A2024/Day12/Day12.php
+++ php/src/A2024/Day12/Day12.php
@@ -81,16 +81,14 @@ class Day12 extends AdventOfCode
         }
     }
 
-    public function new_pos(Direction $case, int $i, int $j): array
+    public function new_pos(Direction $case, int $x, int $y): array
     {
         [$dx, $dy] = $case->neighbor();
-        return [$i - $dx, $j - $dy];
+        return [$x - $dx, $y - $dy];
     }
 
-    public function part1(): int
+    public function getRegions(array $dataset): array
     {
-        $dataset = $this->dataset;
-
         $row = count($dataset);
         $col = count($dataset[0]);
 
@@ -106,13 +104,76 @@ class Day12 extends AdventOfCode
             for ($j = 0; $j < $col; $j++) {
                 $new_region = [];
 
-                $this->traverse($dataset,$regions, $visited, $i, $j, $row, $col, $new_region);
+                $this->traverse($dataset, $regions, $visited, $i, $j, $row, $col, $new_region);
 
                 if (!empty($new_region)) {
                     $regions[] = $new_region;
                 }
             }
         }
+
+        return $regions;
+    }
+
+    public function getCorners(array $region): int
+    {
+        $corners = [
+            [[0, -1], [-1, -1], [-1, 0]],
+            [[-1, 0], [-1, 1], [0, 1]],
+            [[0, 1], [1, 1], [1, 0]],
+            [[1, 0], [1, -1], [0, -1]]
+        ];
+
+        $area = $this->getArea($region);
+
+        $count = 0;
+
+        for ($i = 0; $i < $area; $i++) {
+            $point = $region[$i];
+            foreach ($corners as $corner) {
+                // INNER
+                if (
+                    !in_array($this->new_pos2($point, $corner[0]), $region, true) &&
+                    !in_array($this->new_pos2($point, $corner[1]), $region, true) &&
+                    !in_array($this->new_pos2($point, $corner[2]), $region, true)
+                ) {
+                    $count++;
+                }
+
+                // OUTER
+                if (
+                    in_array($this->new_pos2($point, $corner[0]), $region, true) &&
+                    !in_array($this->new_pos2($point, $corner[1]), $region, true) &&
+                    in_array($this->new_pos2($point, $corner[2]), $region, true)
+                ) {
+                    $count++;
+                }
+
+                if (
+                    !in_array($this->new_pos2($point, $corner[0]), $region, true) &&
+                    in_array($this->new_pos2($point, $corner[1]), $region, true) &&
+                    !in_array($this->new_pos2($point, $corner[2]), $region, true)
+                ) {
+                    $count++;
+                }
+            }
+        }
+
+        return $count;
+    }
+
+    public function new_pos2(array $region, array $corner): array
+    {
+        [$x, $y] = $region;
+        [$dx, $dy] = $corner;
+        return [$x - $dx, $y - $dy];
+    }
+
+    public function part1(): int
+    {
+        $dataset = $this->dataset;
+
+        $regions = $this->getRegions($dataset);
 
         $result = 0;
 
@@ -125,7 +186,17 @@ class Day12 extends AdventOfCode
 
     public function part2(): int
     {
-        return 0;
+        $dataset = $this->dataset;
+
+        $regions = $this->getRegions($dataset);
+
+        $result = 0;
+
+        foreach ($regions as $region) {
+            $result += $this->getArea($region) * $this->getCorners($region);
+        }
+
+        return $result;
     }
 }
 
