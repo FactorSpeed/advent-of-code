8594847c57771db075c924aca270b1c35cbe3782
FactorSpeed <alexis25.py@gmail.com>
2025-09-17 02:49:08 +0200

    [master][2015][Day9]

---
+++ php/src/A2015/Day9/Day9.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Factor\Aoc\A2015\Day9;
+
+require __DIR__ . '/../../AdventOfCode.php';
+require_once __DIR__ . '/Itertools.php';
+
+use Factor\Aoc\AdventOfCode;
+
+class Day9 extends AdventOfCode
+{
+    public array $_routes = [];
+
+    public function readDataset(): void
+    {
+        $dataset = [];
+
+        foreach (file($this->file) as $line) {
+            preg_match('/(\w+) to (\w+) = (\d+)/', $line, $matches);
+            $dataset[] = array_slice($matches, 1);
+        }
+
+        $this->dataset = $dataset;
+    }
+
+    public function getCities(): array
+    {
+        return array_values(
+            array_unique([
+                ...array_column($this->dataset, 0),
+                ...array_column($this->dataset, 1)
+            ])
+        );
+    }
+
+    public function getDistance(string $a, string $b): int
+    {
+        foreach ($this->dataset as $d) {
+            if (
+                ($d[0] === $a && $d[1] === $b) ||
+                ($d[0] === $b && $d[1] === $a)
+            ) {
+                return $d[2];
+            }
+        }
+
+        return 0;
+    }
+
+    public function getCombinations(): array
+    {
+        return new Itertools()->permutations($this->getCities());
+    }
+
+    public function getRoutes(): array
+    {
+        if (count($this->_routes) > 0) {
+            return $this->_routes;
+        }
+
+        foreach ($this->getCombinations() as $combination) {
+            $distance = 0;
+
+            for ($i = 1, $iMax = count($combination); $i < $iMax; $i++) {
+                $distance += $this->getDistance($combination[$i - 1], $combination[$i]);
+            }
+
+            $this->_routes[] = $distance;
+        }
+
+        return $this->_routes;
+    }
+
+    public function part1(): int
+    {
+        return min($this->getRoutes());
+    }
+
+    public function part2(): int
+    {
+        return max($this->getRoutes());
+    }
+}
+
+$day = new Day9(__DIR__);
+$day->run();
---
+++ php/src/A2015/Day9/Itertools.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Factor\Aoc\A2015\Day9;
+
+/**
+ * Based on https://rosettacode.org/wiki/Permutations
+ */
+class Itertools
+{
+    public function permutations($xs): array
+    {
+        $ac = [[]];
+
+        foreach ($xs as $x) {
+            $ac_new = [];
+            foreach ($ac as $ts) {
+                $len = count($ts);
+                for ($n = 0; $n <= $len; $n++) {
+                    $new_ts = $ts;
+                    array_splice($new_ts, $n, 0, $x);
+                    $ac_new[] = $new_ts;
+                }
+            }
+            $ac = $ac_new;
+        }
+
+        return $ac;
+    }
+}
